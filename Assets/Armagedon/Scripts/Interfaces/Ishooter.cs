e73f15827fc13ecaadeabf2ca8de71f8de987bae
DESKTOP-EUEL2TC\Mehrdad <mehrdad.gang@gmail.com>
2017-04-15 07:38:22 -0700

    First

---
+++ Assets/Armagedon/Scripts/Interfaces/Ishooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface Ishooter  {
+    public int CurtentVeponID;
+    public int rt;
+    public List<Weapen> Wepons;
+
+   
+    public enum CType
+    {
+        Player,
+        Enamey,
+        Frends
+        
+    };
+    public Transform AtackPont;
+
+    public bool IsMelee;
+
+	
+   public void ChooseWepons()
+    {
+
+    }
+    public void Shoot()
+    {
+
+    }
+}
